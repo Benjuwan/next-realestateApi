99c4c7755379a944b9d706138679a72a78140116
benjuwan <benjuwan.banquet12@gmail.com>
2024-05-08 15:39:55 +0900

    READMEの更新とコンポーネントへのコメント注釈追記、Topページへ戻る際のデータリセット機能の修正.

--- src/app/components/layout/header/Header.tsx
+++ src/app/components/layout/header/Header.tsx
@@ -4,14 +4,23 @@ import baseStyles from "../../../styles/page.module.css";
 import { memo, useContext } from "react";
 import Link from "next/link";
 import { GetFetchDataContext } from "@/app/providers/filter/GetFetchData";
+import { GetFetchEachCode } from "@/app/providers/filter/GetFetchEachCode";
 
 function Header() {
-    const { setGetFetchData } = useContext(GetFetchDataContext); //「Topへ」の度に取得した不動産データを初期化
+    const { setGetFetchData } = useContext(GetFetchDataContext);
+    const { setGetFetchYearValue, setGetFetchQuarterValue } = useContext(GetFetchEachCode);
+    
+    /* 「Topへ」の度に取得した不動産データを初期化 */
+    const resetAlreadyUseData: () => void = () => {
+        setGetFetchData([]);
+        setGetFetchYearValue((_prevGetFetchYearValue) => '1999');
+        setGetFetchQuarterValue((_prevGetFetchQuarterValue) => '1');
+    }
 
     return (
         <header>
             <div className={baseStyles.breadcrumbs}>
-                <Link href={'/'} onClick={() => setGetFetchData([])}>Topへ</Link>
+                <Link href={'/'} onClick={resetAlreadyUseData}>Topへ</Link>
             </div>
         </header>
     );
